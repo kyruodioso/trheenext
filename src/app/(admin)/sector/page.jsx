d06cc655a60409e0add5bf6ec0746eb6244e7e16
kyruodioso <kanu_nirvana@hotmail.com>
2024-08-02 02:24:28 -0300

    add floor

--- src/app/(admin)/sector/page.jsx
+++ src/app/(admin)/sector/page.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
-import ThreeScene from '@/components/ThreeScene'
-import DataList from '@/components/DataList'
-import Factory from '@/data/factory'
-import Dropdown from '@/components/Dropdown'
-import FactoryPlane from '@/assets/FactoryPlane'
-
+import React from "react";
+import ThreeScene from "@/components/ThreeScene";
+import DataList from "@/components/DataList";
+import Factory from "@/data/factory";
+import Dropdown from "@/components/Dropdown";
+import FactoryPlane from "@/assets/FactoryPlane";
 
 const Sector = () => {
   return (
     <>
-    <div className='grid grid-cols-2 mt-2'>
-      <div className='m-2'>
-        <div>
-        <Dropdown/>
+      {" "}
+      <div className="container mx-auto">
+        <div className="grid grid-cols-2 mt-2">
+          <div className="m-2">
+            <div>
+              <Dropdown />
+            </div>
+            <DataList />
+          </div>
+          <div className="mt-4">
+            <FactoryPlane />
+          </div>
         </div>
-        <DataList/>
       </div>
-      <div className='mt-4'>
-     <FactoryPlane />
-     </div>
-     </div>
     </>
-  )
-}
+  );
+};
 
-export default Sector
+export default Sector;
