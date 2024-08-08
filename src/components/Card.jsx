92c6cbf01cf633ab8486c4079b635c24e0d9ff12
kyruodioso <kanu_nirvana@hotmail.com>
2024-08-08 17:20:40 -0300

    card added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,21 +4,21 @@ import Link from "next/link";
 const Card = ({name,href}) => {
   return (
     <>
-      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
         <img
-          class="w-full"
+          className="w-full"
           src="/img/card-top.jpg"
           alt="Sunset in the mountains"
         />
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">{name}</div>
-          <p class="text-gray-700 text-base text-center">
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">{name}</div>
+          <p className="text-gray-700 text-base text-center">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
             Voluptatibus quia, nulla! Maiores et perferendis eaque,
             exercitationem praesentium nihil.
           </p>
         </div>
-        <div class="px-6 pt-4 pb-2">
+        <div className="px-6 pt-4 pb-2">
           <Link
             href={href}
             className="mt-10 block w-full rounded-full bg-gray-800 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-bg-gray-800"
